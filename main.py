ef3429e05656c31692e430ce94664811b992e378
dimasapriliawan98 <105406824+dimasapriliawan98@users.noreply.github.com>
2022-05-12 17:25:32 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.write("""
+# Menghitung Indeks Massa Tubuh
+Ini adalah aplikasi untuk menghitung Indeks Massa Tubuh menggunakan Streamlit 
+Indeks Massa Tubuh adalah metrik standar yang digunakan untuk menentukan siapa saja yang masuk dalam golongan berat badan sehat dan tidak sehat.
+BMI = (weight in kilograms / height in meters^2  )
+""")
+weight = st.number_input("Masukkan weight", 0)
+height = st.number_input("Masukkan height", 0)
+hitung = st.button("Hitung BMI")
+
+if hitung:
+	BMI = weight / ((height/100)**2)
+	st.write("Hitungan BMInya adalah ", BMI)
+	st.write("BMI dibawah 18,5 dinyatakan Berat Badan Kurang", BMI < 18,5)

7052e81243af53029af0c86e51cf2df105623757
dimasapriliawan98 <105406824+dimasapriliawan98@users.noreply.github.com>
2022-05-13 11:08:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ BMI = (weight in kilograms / height in meters^2  )
 weight = st.number_input("Masukkan weight", 0)
 height = st.number_input("Masukkan height", 0)
 hitung = st.button("Hitung BMI")
-kriteria = st.button("Kriteria BMI")
 
 if hitung:
 	BMI = weight / ((height/100)**2)
 	st.write("Hitungan BMInya adalah ", BMI)
-if BMI < 18,5
+if BMI < 18.5
 	st.write("Berat Badan Kurang")
-if BMI >= 18,5 <= 22,9
+elif BMI >= 18.5 and <= 22.9
 	st.write("Berat Badan Normal")
-if	BMI >= 23 <= 29,9
+elif	BMI >= 23 and <= 29,9
 	st.write("Berat Badan Berlebih (Obesitas)")
-if	BMI > 30
+elif	BMI > 30
 	st.write("Obesitas")
